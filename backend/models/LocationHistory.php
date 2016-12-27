d88c9186c1d3261a16222702409f79f9a90af605
congaductq <ducdvse03458@fpt.edu.vn>
2016-12-27 15:26:56 +0800

    API

--- backend/models/LocationHistory.php
+++ backend/models/LocationHistory.php
@@ -29,6 +29,20 @@ class LocationHistory extends \yii\db\ActiveRecord
         return 'location_history';
     }
 
+    public function behaviors()
+    {
+        return [
+            'timestamp' => [
+                'class' => TimestampBehavior::className(),
+                // Modify only created not updated attribute
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at'],
+                ],
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+
     /**
      * @inheritdoc
      */
