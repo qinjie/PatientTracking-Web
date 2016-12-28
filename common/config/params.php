d44ae4737f52455ad74f22c52533f0e0086598f6
congaductq <ducdvse03458@fpt.edu.vn>
2016-12-28 11:12:54 +0800

    config

--- common/config/params.php
+++ common/config/params.php
@@ -5,6 +5,6 @@ return [
     'supportEmail' => 'mark.qj@gmail.com',
     'user.passwordResetTokenExpire' => 604800,   // 1 week
     'user.emailConfirmUserExpire' => 604800,    // 1 week
-    'locationTimeOut' => '15', //seconds -> if the signal is out of this timeout -> push notification: no signal
+    'locationTimeOut' => '60', //seconds -> if the signal is out of this timeout -> push notification: no signal
     'alertListTimeOut' => '604800', //seconds = 1 week -> only display the notification in nearest 7 days.
 ];
